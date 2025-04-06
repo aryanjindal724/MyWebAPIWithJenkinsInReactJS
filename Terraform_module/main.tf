180c23e0e8bb34120974e2227300df143beaa9c5
Aryan Jindal <aryanjindal455@gmail.com>
2025-04-06 22:33:46 +0530

    first commit

---
+++ Terraform_module/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_app_service_plan" "plan" {
+  name                = var.app_service_plan
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = var.app_service_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.plan.id
+}
---
+++ Terraform_module/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "rg-jenkins"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "app_service_plan" {
+  default = "myplanaryan01"
+}
+
+variable "app_service_name" {
+  default = "reactAppAryan01"
+}
